ea7c874ff196d1d689d20011ee9a93423700ae92
saucepommefrite <99143550+saucepommefrite@users.noreply.github.com>
2023-12-01 13:52:02 +0100

    :sparkles: Code refactoring - Day 1 - Part 1

--- AdventOfCode/day-1/main-day-1.cpp
+++ AdventOfCode/day-1/main-day-1.cpp
@@ -4,44 +4,36 @@
 #include <vector>
 
 int concatInteger(int a, int b){
-    std::string inta=std::to_string(a);
-    std::string intb=std::to_string(b);
-
-    std::string concatInt = inta + intb;
-    return std::stoi(concatInt);
+    return std::stoi(std::to_string(a) + std::to_string(b));
 }
 
 int main(){
     std::ifstream file;
     std::vector<int>temparray;
     std::vector<int>result;
-    file.open("/Users/luevard/CLionProjects/AdventOfCodeGithub/AdventOfCode/day-1/input.txt");
+    int final=0;
     std::string buffer;
 
+    file.open("/Users/luevard/CLionProjects/AdventOfCodeGithub/AdventOfCode/day-1/input.txt");
     if(!file.is_open()) {
         std::cout<< "Erreur pendant l'ouverture du fichier"<<std::endl;
         return -1;
     }
-
+    
     while(!file.eof()){
         file >> buffer;
         for(char& c : buffer) {
-            if((int)c>47 && (int)c<58) {
+            if(std::isdigit(c)) 
                 temparray.push_back(digittoint(c));
-            }
         }
         if(!temparray.empty()){
             result.push_back(concatInteger(temparray.front(),temparray.back());
             temparray.clear();
         }
     }
-
-    int final=0;
     for(int i : result){
         final=final+i;
     }
-
     std::cout<<final;
-
     return 0;
 }
