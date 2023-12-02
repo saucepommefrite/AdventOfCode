8001abefcec5e5a74179234d38a074fe1f1f05c1
saucepommefrite <99143550+saucepommefrite@users.noreply.github.com>
2023-12-02 09:15:54 +0100

    :sparkles: Add Day-1 Part 2

--- AdventOfCode/day-1/main-day-1.cpp
+++ AdventOfCode/day-1/main-day-1.cpp
@@ -2,38 +2,70 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <numeric>
+
+int checkWord(const std::string& buffer) {
+    static const std::map<std::string, int> compareString = {
+            {"one", 1},
+            {"two", 2},
+            {"three", 3},
+            {"four", 4},
+            {"five", 5},
+            {"six", 6},
+            {"seven", 7},
+            {"eight", 8},
+            {"nine", 9}
+    };
+    for (const auto& [key, value] : compareString) {
+        if (buffer.find(key) != std::string::npos) {
+            return value;
+        }
+    }
+    return -1;
+}
+
 
 int concatInteger(int a, int b){
     return std::stoi(std::to_string(a) + std::to_string(b));
 }
 
 int main(){
-    std::ifstream file;
-    std::vector<int>temparray;
-    std::vector<int>result;
-    int final=0;
-    std::string buffer;
 
-    file.open("/Users/luevard/CLionProjects/AdventOfCodeGithub/AdventOfCode/day-1/input.txt");
-    if(!file.is_open()) {
-        std::cout<< "Erreur pendant l'ouverture du fichier"<<std::endl;
+    std::ifstream file("/Users/luevard/CLionProjects/AdventOfCodeGithub/AdventOfCode/day-1/input.txt");
+    if (!file.is_open()) {
+        std::cout << "Erreur pendant l'ouverture du fichier" << std::endl;
         return -1;
     }
+
+    std::vector<int> intArray;
+    std::vector<int>result;
+    std::vector<char>buffer;
+    std::string bufferFile;
     
     while(!file.eof()){
-        file >> buffer;
-        for(char& c : buffer) {
-            if(std::isdigit(c)) 
-                temparray.push_back(digittoint(c));
+        file >> bufferFile;
+        for(char& c : bufferFile) {
+
+            if(std::isdigit(c)) {
+                buffer.clear();
+                intArray.push_back(digittoint(c));
+            }
+            else buffer.push_back(c);
+
+            std::string str(buffer.begin(),buffer.end());
+            int response = checkWord(str);
+            if (response > 0) {
+                buffer.clear();
+                buffer.push_back(str.back()); // Exemple: Threeight => 3 8
+                intArray.push_back(response);
+            }
         }
-        if(!temparray.empty()){
-            result.push_back(concatInteger(temparray.front(),temparray.back());
-            temparray.clear();
+        if(!intArray.empty()){
+            result.push_back(concatInteger(intArray.front(),intArray.back()));
+            intArray.clear();
         }
     }
-    for(int i : result){
-        final=final+i;
-    }
-    std::cout<<final;
+    std::cout << std::accumulate(result.begin(), result.end(), 0);
     return 0;
 }
